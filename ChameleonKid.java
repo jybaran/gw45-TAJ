5e3255256c406dcb717ec672ba8a72ef51f99fde
Jenny Baran <jenny.baran@gmail.com>
2014-04-30 19:56:39 -0400

    exercise 2
    chameleonkid

---
+++ ChameleonKid.java
@@ -0,0 +1,45 @@
+// JENNY BARAN
+// APCS2 pd 9
+// HW34
+// 2014-04-29
+
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Critter;
+import info.gridworld.grid.Location;
+import java.util.ArrayList;
+
+/**
+ * A ChameleonKid takes on the color of neighboring actors in front/back of it as it moves through the grid.
+ */
+public class ChameleonKid extends ChameleonCritter {
+    /**
+     * Gets actors for processing.
+     */
+    public ArrayList<Actor> getActors() {
+	ArrayList<Actor> actors = new ArrayList<Actor>();
+	int[] dirs = { Location.AHEAD, Location.HALF_CIRCLE };
+	for (Location loc: getLocationsInDirections(dirs)) {
+	    Actor a = getGrid().get(loc);
+	    if (a != null) {
+		actors.add(a);
+	    }
+	}
+	return actors;
+    }
+    
+    /**
+     * Finds valid adj locs of this critter
+     */
+    public ArrayList<Location> getLocationsInDirections(int[] directions) {
+	ArrayList<Location> locs = new ArrayList<Location>();
+	Grid gr = getGrid();
+	Location loc = getLocation();
+	for (int d : directions) {
+	    Location neighborLoc = loc.getAdjacentLocation(getDirection() + d);
+	    if (gr.isValid(neighborLoc)) {
+		locs.add(neighborLoc);
+	    }
+	}
+	return locs;
+    }
+}//end class ChameleonKid
